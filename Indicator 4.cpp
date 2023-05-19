ae004d231c5451a63bdea08efe6f07d382b6aeb7
Sudhansu Patra <130463585+Sudhansu14@users.noreply.github.com>
2023-05-19 17:08:58 +0530

    Indicator 4.cpp

--- Indicator 4.cpp
+++ Indicator 4.cpp
@@ -2,30 +2,32 @@
 
 main()
 {
-
-  int*a,*b
- 
- {
-    int temp=*a;
-    *a=*b;
-    *b=temp;
- }
- 
-
-  reverse(int array[], int array_size)
-  {
-    
-    int *pointer1 =array,
- 
-        *pointer2 =array+array_size-1;
-        
-    while (pointer1<pointer2) 
+	int a[100],n,i;
+	int *p;
+	p= &a;
+	printf("enter value of N:");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
 	{
-        swap(pointer1,pointer2);
-        pointer1++;
-        pointer2--;
-    }
- }
-
+		printf("a[%d]=",i);
+		scanf("%d",&a[i]);
+		
+	}
+	
+	printf("element in array arr:");
+	for(i=0;i<n;i++)
+	
+	{
+		printf("%d",*p+i)
+        }
+	
+	printf("\nrevers array:");
+	rev=&a[n-1];
+	
+	for(i=0;i<n;i++)
+	{
+		printf("%d",*rev--);
+	}	
 }
  
