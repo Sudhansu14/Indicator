3c6626c8826a2371960bd693becb8b2f784afdd3
Sudhansu Patra <130463585+Sudhansu14@users.noreply.github.com>
2023-05-19 17:22:33 +0530

    Indicator 3.cpp

--- Indicator 3.cpp
+++ Indicator 3.cpp
@@ -1,28 +1,8 @@
 #include<stdio.h>
-
-int string_ln(char*);
- 
-main() 
+main()
 {
-   char str[20];
-   int length;
-   
-   
-   printf("\nEnter any string : ");
-   gets(str);
-   
-   length = string_ln(str);
-   printf("The length of the given string %s is : %d", str, length);
-
-}
- 
-int string_ln(char*p)
-{
-   int count=0;
-   while (*p!='\0') 
-   {
-      count++;
-      p++;
-   }
-
-}
+   char string;
+   char*p;
+ printf("enter your string");
+ p&a;
+ printf("string length =%d",strlen(name));
